151ec08c20781d61ac73b9bb228245ec2cc922e7
Marco Fernando <marcofernando.dev@gmail.com>
2026-02-26 15:10:42 +0530

    chore: resolve conflict in db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
 module.exports = async function connectDB() {
-   const uri = process.env.MONGO_URI;
-   if (!uri) throw new Error("MOngo_URI is missing in .env file");
+  const uri = process.env.MONGO_URI;
+  if (!uri) throw new Error("MONGO_URI is missing in .env file");
 
-   await mongoose.connect(uri);
-   console.log("MOngoDB connected");
-
-
-}
+  try {
+    await mongoose.connect(uri);
+    console.log("MongoDB connected successfully!");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);  // Exit the process if connection fails
+  }
+};
